f20af6aa329e5e834bb815e305173a4b2e584800
Tao-68 <tanyuntao99@gmail.com>
2025-05-11 21:34:33 +0200

    update bcrypt tasks

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 'use strict';
 const express     = require('express');
 const bodyParser  = require('body-parser');
+const bcrypt = require('bcrypt');
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
 const app         = express();
 fccTesting(app);
@@ -10,14 +11,22 @@ const someOtherPlaintextPassword = 'pass123';
 
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
-
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+    console.log(hash);
+    bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+      console.log(res);
+    });
+  });
 
 
 //END_ASYNC
 
 //START_SYNC
 
-
+var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+console.log(hash);
+var result = bcrypt.compareSync(myPlaintextPassword, hash);
+console.log(result);
 
 //END_SYNC
 
